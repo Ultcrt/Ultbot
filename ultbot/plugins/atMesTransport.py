63621fce092e5436adbaa84ee47f54cda9661bf6
Ultcrt <zhang_zhe_yuan@outlook.com>
2019-07-04 23:02:29 +0800

    @消息转发脚本

--- ultbot/plugins/atMesTransport.py
+++ ultbot/plugins/atMesTransport.py
@@ -1,7 +1,17 @@
 import nonebot
+from nonebot.typing import Context_T
+from aiocqhttp.exceptions import Error as CQHttpError
+import re
 
 bot = nonebot.get_bot()
 
+
 @bot.on_message('group')
-async def _(ctx:Context_T):
-    
+async def _(ctx: Context_T):
+    if '[CQ:at,qq=326090231]' in ctx['raw_message']:
+        try:
+            await bot.send_private_msg(user_id=326090231,
+                                       message=ctx['raw_message'])
+
+        except CQHttpError:
+            pass
