cb6c7453c2be151a4611014d0ec91514522b5da4
Ultcrt <zhang_zhe_yuan@outlook.com>
2019-07-05 12:09:44 +0800

    Update atMesTransport.py

--- ultbot/plugins/atMesTransport.py
+++ ultbot/plugins/atMesTransport.py
@@ -11,7 +11,8 @@ async def _(ctx: Context_T):
     if '[CQ:at,qq=326090231]' in ctx['raw_message']:
         try:
             await bot.send_private_msg(user_id=326090231,
-                                       message=ctx['raw_message'])
+                                       message='From'+ctx['user_id']
+                                               +':\n'+ctx['raw_message'])
 
         except CQHttpError:
             pass
