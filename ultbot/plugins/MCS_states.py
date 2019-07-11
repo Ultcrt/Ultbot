b048ebaaef241f77b6262e72a545c1dc77ba7a8c
Ultcrt <zhang_zhe_yuan@outlook.com>
2019-07-11 18:37:00 +0800

    Update MCS_states.py

--- ultbot/plugins/MCS_states.py
+++ ultbot/plugins/MCS_states.py
@@ -6,17 +6,16 @@ from aiocqhttp.exceptions import Error as CQHttpError
 @on_command('mcsinfo', only_to_me=False)
 async def mcsinfo(session: CommandSession):
     result = os.popen('netstat  -anp  |grep 25565')
-    for string in result:
-        if 'LISTEN' in string:
-            try:
-                await session.send('Minecraft服务端运行中')
-            except CQHttpError:
-                pass
-        else:
-            try:
-                await session.send('Minecraft服务端目前停止运行')
-            except CQHttpError:
-                pass
+    if not result:
+        try:
+            await session.send('Minecraft服务端运行中')
+        except CQHttpError:
+            pass
+    else:
+        try:
+            await session.send('Minecraft服务端目前停止运行')
+        except CQHttpError:
+            pass
     try:
         await session.send('如果无法访问服务器，可以尝试输入‘/mcsreboot’重启Minecraft服务端\n'
                            '警告：请核实服务器内是否有其他玩家再进行重启\n'
