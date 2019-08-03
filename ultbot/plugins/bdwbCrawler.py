86b37c0f6b8641dec26886c7f2f51adff8f8dd0e
Ultcrt <zhang_zhe_yuan@outlook.com>
2019-08-03 20:37:08 +0800

    convert to package

--- ultbot/plugins/bdwbCrawler.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def crawler():
-    url = 'https://weibo.cn/u/6314659177'
-    temp = requests.get(url, cookies=cookie)
-    with open('tmp.html', 'w') as f:
-        f.write(temp.content.decode('utf-8', 'ignore'))
---
+++ ultbot/plugins/bdwbCrawler/__init__.py
@@ -0,0 +1,13 @@
+import requests
+from cookies import weibo_cookie
+
+
+def crawler():
+    url = 'https://weibo.cn/u/6314659177'
+    temp = requests.get(url, cookies=weibo_cookie)
+    with open('tmp.html', 'w', encoding='utf-8') as f:
+        f.write(temp.content.decode('utf-8', 'ignore'))
+
+
+if __name__ == '__main__':
+    crawler()
