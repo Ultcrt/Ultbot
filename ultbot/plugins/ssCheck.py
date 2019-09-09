36b80adb23b676d6bdfc248ca777cc5dc7b2af13
Ultcrt <zhang_zhe_yuan@outlook.com>
2019-09-09 10:45:03 +0800

    ss update

--- ultbot/plugins/ssCheck.py
+++ ultbot/plugins/ssCheck.py
@@ -6,7 +6,7 @@ from aiocqhttp.exceptions import Error as CQHttpError
 
 @on_command('ssping', only_to_me=False)
 async def ssping(session: CommandSession):
-    ip = '45.77.25.40'
+    ip = '155.138.143.151'
     bot = nonebot.get_bot()
     await session.send('测试可能需要0到30s\n请耐心等待')
     temp = os.popen('ping -c 5 -w 6 %s' % ip)
